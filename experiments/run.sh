65d95754f806af219d4a3106b09d6a254525574c
pl-eflect <pl.eflect@gmail.com>
2021-03-09 19:42:38 -0700

    update to run script

--- experiments/run.sh
+++ experiments/run.sh
@@ -9,7 +9,7 @@ BENCHMARKS=(biojava jython xalan)
 
 for CORUNS in `seq 2 1 5`; do
   for BENCHMARK in ${BENCHMARKS[@]}; do
-    ./scripts/dacapo-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/same/$CORUNS/$BENCHMARK
+    ./scripts/dacapo-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
   done
 done
 
@@ -18,7 +18,7 @@ BENCHMARKS=(avrora batik eclipse h2 pmd sunflow)
 
 for CORUNS in `seq 2 1 5`; do
   for BENCHMARK in ${BENCHMARKS[@]}; do
-    ./scripts/dacapo-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/same/$CORUNS/$BENCHMARK
+    ./scripts/dacapo-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
   done
 done
 
@@ -28,19 +28,19 @@ SIZE=default
 BENCHMARKS=(biojava jython xalan)
 
 for BENCHMARK in ${BENCHMARKS[@]}; do
-  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/same/$CORUNS/$BENCHMARK
+  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
 done
 
 SIZE=large
 BENCHMARKS=(avrora batik eclipse h2 pmd sunflow)
 
 for BENCHMARK in ${BENCHMARKS[@]}; do
-  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/same/$CORUNS/$BENCHMARK
+  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
 done
 
 for CORUNS in `seq 2 1 5`; do
-  ./scripts/tensorflow-same-co-run.sh $CORUNS $OUTPUT_DIR/same/$CORUNS/tensorflow
+  ./scripts/tensorflow-same-co-run.sh $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/tensorflow
 done
 
-./scripts/sunflow-same-co-run.sh "50 50" $OUTPUT_DIR/same/sunflow
+./scripts/sunflow-same-co-run.sh "50 50" $OUTPUT_DIR/footprints/same/sunflow
 ./scripts/sunflow-same-co-run.sh "30 70" $OUTPUT_DIR/mixed/sunflow
