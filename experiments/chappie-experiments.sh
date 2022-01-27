b72277610f1094ec3eab7352cf079b31d3dbe40c
pl-eflect <pl-eflect@gmail.com>
2022-01-26 19:37:41 -0500

    normalized paths

--- experiments/chappie-experiments.sh
+++ experiments/chappie-experiments.sh
@@ -9,12 +9,12 @@ SIZE=default
 BENCHMARKS=(biojava jython xalan)
 
 for BENCHMARK in ${BENCHMARKS[@]}; do
-  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
+  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/$BENCHMARK
 done
 
 SIZE=large
 BENCHMARKS=(avrora batik eclipse graphchi h2 pmd sunflow)
 
 for BENCHMARK in ${BENCHMARKS[@]}; do
-  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/footprints/same/$CORUNS/$BENCHMARK
+  ./scripts/dacapo-chappie-same-co-run.sh "$BENCHMARK -s $SIZE -n $ITERS" $CORUNS $OUTPUT_DIR/$BENCHMARK
 done
